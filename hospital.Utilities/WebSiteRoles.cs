d654a4d9c6c9e0ce63fb4a4f5f0e8182c391f8d6
carlosbernardocharles <carlosbernardocharles@gmail.com>
2023-11-17 13:14:02 +0200

    start

---
+++ hospital.Utilities/WebSiteRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hospital.Utilities
+{
+    public static class WebSiteRoles
+    {
+        public const string WebSite_Admin = "Admin";
+        public const string WebSite_Paciente = "Paciente";
+        public const string WebSite_Medico = "Medico";
+    }
+}
